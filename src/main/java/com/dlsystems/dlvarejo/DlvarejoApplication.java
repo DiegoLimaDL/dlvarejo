e46737a39eab96ddf62f7e43c461fa3d2d5fc8bd
Diego Lima <diegoconcert@gmail.com>
2020-11-09 16:44:57 -0300

    Rest Testado, mundança no nome da aplicação de Dl para DL

--- src/main/java/com/dlsystems/dlvarejo/DlvarejoApplication.java
+++ src/main/java/com/dlsystems/dlvarejo/DlvarejoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DlvarejoApplication {
+public class DLvarejoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DlvarejoApplication.class, args);
+		SpringApplication.run(DLvarejoApplication.class, args);
 	}
 
 }
