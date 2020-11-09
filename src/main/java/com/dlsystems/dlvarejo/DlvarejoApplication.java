daeabaa60dc6a8bdf237215ffbcb2dfd95b5f489
Diego Lima <diegoconcert@gmail.com>
2020-11-09 14:52:37 -0300

    Projeto criado

---
+++ src/main/java/com/dlsystems/dlvarejo/DlvarejoApplication.java
@@ -0,0 +1,13 @@
+package com.dlsystems.dlvarejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlvarejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlvarejoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlsystems/dlvarejo/DlvarejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlsystems.dlvarejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlvarejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
